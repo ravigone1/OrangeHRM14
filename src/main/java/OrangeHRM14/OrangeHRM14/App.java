7a3d90a2b9333056f49d071f31c842fc74ba7c44
ravigone1 <ravigon848@gmail.com>
2021-09-06 14:56:31 +0530

    second Project

---
+++ src/main/java/OrangeHRM14/OrangeHRM14/App.java
@@ -0,0 +1,13 @@
+package OrangeHRM14.OrangeHRM14;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
